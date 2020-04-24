b607207cd3a477b59dd189b8cc09aa74873746ed
15711657500 <15711657500@163.com>
2020-04-24 17:11:32 +0800

    first commit

--- srcs/library.go
+++ srcs/library.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	// mysql connector
+	"crypto/sha256"
+	"encoding/hex"
 	_ "github.com/go-sql-driver/mysql"
 	sqlx "github.com/jmoiron/sqlx"
 )
@@ -18,6 +20,13 @@ type Library struct {
 	db *sqlx.DB
 }
 
+func getSHA256(input string) string {
+	hash := sha256.New()
+	hash.Write([]byte(input))
+	bytes := hash.Sum(nil)
+	hashCode := hex.EncodeToString(bytes)
+	return hashCode
+}
 func (lib *Library) ConnectDB() {
 	db, err := sqlx.Open("mysql", fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s", User, Password, DBName))
 	if err != nil {
@@ -28,7 +37,21 @@ func (lib *Library) ConnectDB() {
 
 // CreateTables created the tables in MySQL
 func (lib *Library) CreateTables() error {
-	return nil
+	_, err := lib.db.Exec(`
+drop table if exists book;
+create table if not exists book
+(
+    title     varchar(50),
+    author    varchar(50),
+    ISBN      varchar(100) primary key,
+    total     int,
+    constraint tcs check (total >= 0),
+    remaining int,
+    constraint rcs check (remaining <= total and remaining >= 0)
+)
+;
+	`)
+	return err
 }
 
 // AddBook add a book into the library
