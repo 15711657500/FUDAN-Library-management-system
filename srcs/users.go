780a6ef40e7e8a10f11046ab3dc6cdfe7eeb46eb
15711657500 <15711657500@163.com>
2020-04-24 18:16:32 +0800

    modify

--- srcs/users.go
+++ srcs/users.go
@@ -2,13 +2,13 @@ package main
 
 func resetusers(lib *Library) error {
 	_, err := lib.db.Exec(`
-drop table if exists user;
+drop table if exists users;
 `)
 	return err
 }
 func createusers(lib *Library) error {
 	_, err := lib.db.Exec(`
-	create table if not exists user
+	create table if not exists users
 (
     username varchar(50) primary key,
     password varchar(50),
